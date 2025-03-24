3bda43c493b9d69432faf6b5c559d7ba93075e09
Nguyễn Sỹ Trường <sytruong1812@gmail.com>
2025-03-24 20:13:26 +0700

    Teddy

--- cURL_Adapter_WinAPI/libcurl_adapter/setopt.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-#include "curl.h"
-
-CURLcode Curl_setstropt(char** charp, const char* s);
-CURLcode Curl_setblobopt(struct curl_blob** blobp, const struct curl_blob* blob);
-CURLcode Curl_vsetopt(struct Curl_easy* data, CURLoption option, va_list arg);
